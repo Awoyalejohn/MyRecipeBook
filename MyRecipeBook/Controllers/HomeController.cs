3944fcb53ff768061be9201859908953a8c1171b
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2023-03-25 18:50:04 +0000

    Setup project for MVC

---
+++ MyRecipeBook/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Models;
+using System.Diagnostics;
+
+namespace MyRecipeBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Privacy() => View();
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- MyRecipeBook/Program.cs
+++ MyRecipeBook/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("Home/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
 
 app.Run();

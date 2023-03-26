a194973be560ead57e6cf26913ac155a431f3fb1
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2023-03-26 15:35:11 +0100

    Add recipe model

---
+++ MyRecipeBook/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyRecipeBook.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string Cuisine { get; set; } = null!;
+        public int Serves { get; set; }
+        public List<string> Ingredients { get; set; } = null!;
+        public string Steps { get; set; } = null!;
+        public DateTime PreparationTime { get; set; }
+        public DateTime CookTime { get; set; }
+    }
+}

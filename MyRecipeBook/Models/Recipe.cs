e07fcf8ea69f2e68b6a13972d01b99b6f1ed06fd
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2023-03-26 16:15:54 +0100

    Change prep time and cook time properties to timespans and add validation attributes

--- MyRecipeBook/Models/Recipe.cs
+++ MyRecipeBook/Models/Recipe.cs
@@ -6,14 +6,24 @@ namespace MyRecipeBook.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Name { get; set; } = null!;
+
+        [StringLength(250, MinimumLength = 3)]
         public string Description { get; set; } = null!;
+
+        [StringLength(50, MinimumLength = 3)]
         public string Category { get; set; } = null!;
+
+        [StringLength(50, MinimumLength = 3)]
         public string Cuisine { get; set; } = null!;
+
+        [Range(1, 10)]
         public int Serves { get; set; }
         public List<string> Ingredients { get; set; } = null!;
         public List<string> Steps { get; set; } = null!;
-        public DateTime PreparationTime { get; set; }
-        public DateTime CookTime { get; set; }
+        public TimeSpan PreparationTime { get; set; }
+        public TimeSpan CookTime { get; set; }
     }
 }

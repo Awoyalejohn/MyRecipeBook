ca93c309aa6ad30a8e4fa271f27c6fac441aa63a
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2023-04-09 23:05:42 +0100

    Change client side packages to cdns instead and publish deploy website

--- MyRecipeBook/Program.cs
+++ MyRecipeBook/Program.cs
@@ -45,7 +45,7 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipline
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("Home/Error");
+    app.UseExceptionHandler("/Home/Error");
 
     app.UseHsts();
 }
